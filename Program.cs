b09dd62a6d19dd2c05d5dd66305afe8c50179306
Sean Dorris <seanc.dorris@gmail.com>
2023-07-12 10:52:05 -0500

    testing repo creation from VScode

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+
+Console.WriteLine(greeting);
+
+Console.WriteLine("Please enter a product name: ");
+string? response = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(response))
+{
+    Console.WriteLine("You didn't choose anything!");
+}
+else
+{
+    Console.WriteLine($"You chose: {response}");
+}
